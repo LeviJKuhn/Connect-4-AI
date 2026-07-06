b8121f13f714b0bd29ec3f1ba258890235a7f97a
LeviJKuhn <ljkuhn33@gmail.com>
2026-07-06 12:27:33 -0700

    Encapsulate Methods Implement Local 1-vs-1 Game

--- main.cpp
+++ main.cpp
@@ -5,18 +5,71 @@
 
 #include "Board.h"
 
+#include <string>
+#include <algorithm>
+#include <cctype>
+
+void TestCreation();
+void LocalGame();
+void PlayAgainstAI();
+
 int main()
 {
-    /* Instantiate Machine
-     * Begin Playing
-     *
-     * Test Constructor Injection + Postponed Instantiation
-     */
+    // TestCreation();
 
+    LocalGame();
+
+    // PlayAgainstAI();
+
+    return 0;
+}
+
+
+void TestCreation()
+{
     Board board;
     for (int i = 1; i <= 7; i++)
         board.Move(i);
     board.PrintBoard();
+}
 
-    return 0;
+void LocalGame()
+{
+    Board board;
+    bool Playing = true;
+    bool GameOver = false;
+    bool PlayerOnesTurn = true;
+    int PlacedPiece;
+    string Response;
+
+    while (Playing)
+    {
+        if (PlayerOnesTurn)
+            cout << "Select a coloumn Player One: ";
+        else
+            cout << "Select a coloumn Player Two: ";
+        cin >> PlacedPiece;
+        GameOver = board.Move(PlacedPiece);
+        board.PrintBoard();
+
+        if (GameOver)
+        {
+            cout << "Game Over!" << endl;
+            while (Response != "Y" && Response != "YES" && Response != "N" && Response != "NO"){
+                cout << "Would you like to play again? (Y/N): ";
+                cin >> Response;
+                transform(Response.begin(), Response.end(), Response.begin(), ::toupper);
+            }
+            if (Response == "N" || Response == "NO")
+                Playing = false;
+            Response = "";
+        }
+
+        PlayerOnesTurn = !PlayerOnesTurn;
+    }
+}
+
+void PlayAgainstAI()
+{
+    return;
 }
